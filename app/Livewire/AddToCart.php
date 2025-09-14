c6bca5f6921bfd8ee834f27870ebe339188fb2a3
zaki-ramadhan <zakiram4dhan@gmail.com>
2025-09-14 22:57:59 +0700

    feat: add addToCart feature and its validation

--- app/Livewire/AddToCart.php
+++ app/Livewire/AddToCart.php
@@ -23,9 +23,21 @@ class AddToCart extends Component
         $this->stock = $product->stock;
         $this->weight = $product->weight;
         $this->quantity = $cart->getItemBySku($product->sku)->quantity ?? 1;
+        
+        $this->validate();
+    }
+
+    // add rules for quantity cart item input
+    public function rules() : array {
+        return [
+            'quantity' => ['required', 'integer', 'min:1', "max:{$this->stock}"]
+        ];
     }
     
-    public function addToCart(CartServiceInterface $cart) {
+    public function addToCart(CartServiceInterface $cart)
+    {
+        $this->validate();
+        
         $cart->addOrUpdate(new CartItemData(
             sku: $this->sku,
             quantity: $this->quantity,
@@ -33,7 +45,7 @@ class AddToCart extends Component
             weight: $this->weight,
         ));
     }
-    
+
     public function render()
     {
         return view('livewire.add-to-cart');
